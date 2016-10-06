7065b5d153d80fda1e51cc9f74c9c3302f2aa04e
Romeli <Romeli@10.0.0.200>
2016-10-07 03:34:47 +0800

    Update SPI2

--- inc/SPI.h
+++ inc/SPI.h
@@ -13,8 +13,13 @@
 
 #include "stm32f10x.h"
 
-#define SPI_Speed SPI_Speed_18M		//only on SPI1
+#define SPI_CH SPI2
+//#define USE_SPI1
+#define USE_SPI2
 
+#define SPI_Speed SPI_Speed_2_25M		//only on SPI1
+
+#ifdef USE_SPI1
 #define SPI_Speed_18M SPI_BaudRatePrescaler_4
 #define SPI_Speed_9M SPI_BaudRatePrescaler_8
 #define SPI_Speed_4_5M SPI_BaudRatePrescaler_16
@@ -22,11 +27,19 @@
 #define SPI_Speed_1_125M SPI_BaudRatePrescaler_64
 #define SPI_Speed_562_5K SPI_BaudRatePrescaler_128
 #define SPI_Speed_281_25K SPI_BaudRatePrescaler_256
+#else
+#define SPI_Speed_18M SPI_BaudRatePrescaler_2
+#define SPI_Speed_9M SPI_BaudRatePrescaler_4
+#define SPI_Speed_4_5M SPI_BaudRatePrescaler_8
+#define SPI_Speed_2_25M SPI_BaudRatePrescaler_16
+#define SPI_Speed_1_125M SPI_BaudRatePrescaler_32
+#define SPI_Speed_562_5K SPI_BaudRatePrescaler_64
+#define SPI_Speed_281_25K SPI_BaudRatePrescaler_128
+#endif
 
-class SPIClass
-{
+class SPIClass {
 public:
-	void init();
+	void Init();
 	uint8_t transfer(uint8_t data);
 };
 
--- src/SPI.cpp
+++ src/SPI.cpp
@@ -8,34 +8,41 @@
 
 SPIClass SPI;
 
-void SPI_GPIO_Init()
-{
+void SPI_GPIO_Init() {
 	GPIO_InitTypeDef GPIO_InitStructure;
-
+#ifdef USE_SPI1
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
 
-	//SPI CLK
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5; //SPI CLK
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	//SPI MISO
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
-
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;//SPI MISO
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	//SPI MOSI
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
-
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;//SPI MOSI
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
+#endif
+#ifdef USE_SPI2
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE);
 
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13; //SPI CLK
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14; //SPI MISO
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15; //SPI MOSI
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+#endif
 }
 
-void SPIClass::init()
-{
+void SPIClass::Init() {
 	SPI_InitTypeDef SPI_InitStructure;
 
 	SPI_GPIO_Init();
@@ -43,22 +50,23 @@ void SPIClass::init()
 	SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex; //双线双向全双工
 	SPI_InitStructure.SPI_Mode = SPI_Mode_Master; //设置为主SPI
 	SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b; //8位帧结构
-	SPI_InitStructure.SPI_CPOL = SPI_CPOL_High; //时钟悬空高
-	SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge; //第二个时钟沿
+	SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low; //时钟悬空高
+	SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge; //第二个时钟沿
 	SPI_InitStructure.SPI_NSS = SPI_NSS_Soft; //内部NSS信号由SSI位控制
 	SPI_InitStructure.SPI_BaudRatePrescaler = SPI_Speed; //预分频值为2
 	SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB; //数据传输从MSB位开始
 	SPI_InitStructure.SPI_CRCPolynomial = 7; //用于CRC值计算的多项式
-	SPI_Init(SPI1, &SPI_InitStructure); //初始化配置
-	SPI_Cmd(SPI1, ENABLE);
+
+	SPI_Init(SPI_CH, &SPI_InitStructure); //初始化配置
+	SPI_Cmd(SPI_CH, ENABLE);
+
 }
 
-uint8_t SPIClass::transfer(uint8_t data)
-{
-	SPI_I2S_SendData(SPI1, data);
-	while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET)
+uint8_t SPIClass::transfer(uint8_t data) {
+	SPI_I2S_SendData(SPI_CH, data);
+	while (SPI_I2S_GetFlagStatus(SPI_CH, SPI_I2S_FLAG_TXE) == RESET)
 		//等待时钟结束，以便接收数据
 		;
-	return SPI_I2S_ReceiveData(SPI1);
+	return SPI_I2S_ReceiveData(SPI_CH);
 }
 
