2406d4304d72d6dc97bc92df84225254840df829
Romeli <Romeli@10.0.0.200>
2016-10-08 17:15:09 +0800

    21

--- src/PID.cpp
+++ src/PID.cpp
@@ -7,8 +7,9 @@
 
 #include "PID.h"
 
-PIDClass::PIDClass(double p, double i, double d, double set, double *now,
-		double *out, double min, double max, PIDMode mode) {
+PIDClass::PIDClass(double p, double i, double d, double *set, double *now,
+		double *out, double min, double max, PIDMode mode)
+{
 	Kp = p;
 	Ki = i;
 	Kd = d;
@@ -18,36 +19,49 @@ PIDClass::PIDClass(double p, double i, double d, double set, double *now,
 	OutMin = min;
 	OutMax = max;
 	Mode = mode;
+
+	DTerm = 0;
+	pError = 0;
+	Last = 0;
+	iTerm = 0;
 }
 
-void PIDClass::Compute() {
-	pError_Last = pError;									//Get last pError
-	pError = Set - *Now;							//Compute pError
-	iTerm += Ki * pError;										//Compute iTerm
+void PIDClass::Compute()
+{
+	pError = *Set - *Now;							//Compute pError
+	iTerm += Ki * pError;								//Compute iTerm
 	iTerm = iTerm > OutMax ? OutMax :		//Limit of iTerm
 			iTerm < OutMin ? OutMin : iTerm;
-	dError = pError_Last - pError;								//Compute dError
-	if (Mode == PIDMode_Diff) {
-		*Out += Kp * pError + iTerm + Kd * dError + *Out;
-	} else {
-		*Out = Kp * pError + iTerm + Kd * dError;
+	DTerm = Kd * (*Now - Last);						//Compute DTerm
+	Last = *Now;									//Get last pError
+	if (Mode == PIDMode_Diff)
+	{
+		*Out = Kp * pError + iTerm - DTerm + *Out;
+	}
+	else
+	{
+		*Out = Kp * pError + iTerm - DTerm;
 	}
 	*Out = *Out > OutMax ? OutMax :						//Limit of Output_Inside
 			*Out < OutMin ? OutMin : *Out;
 }
 
-void PIDClass::Compute(double set) {
-	Set = set;
-	Compute();
-}
-
-void PIDClass::SetTunings(double p, double i, double d) {
+void PIDClass::SetTunings(double p, double i, double d)
+{
 	Kp = p;
 	Ki = i;
 	Kd = d;
 }
 
-void PIDClass::SetOutputLimits(double min, double max) {
+void PIDClass::SetOutputLimits(double min, double max)
+{
 	OutMin = min;
 	OutMax = max;
+}
+
+void PIDClass::Clear()
+{
+	*Out = 0;
+	iTerm = 0;
+	Last = 0;
 }
